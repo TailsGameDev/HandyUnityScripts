497090dc7d1c97501a8e06d895cf943645d77555
Caina Correa Caldas <cainacc@hotmail.com>
2021-10-17 18:15:38 -0300

    ObjectDestroyer

---
+++ ObjectDestroyer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+    private static ObjectDestroyer instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static void DestroyObjectInTime(GameObject gameObjectToDestroy, float timeToDestruction)
+    {
+        instance.StartCoroutine(instance.DestructionCoroutine(gameObjectToDestroy, timeToDestruction));
+    }
+    private IEnumerator DestructionCoroutine(GameObject gameObjectToDestroy, float timeToDestruction)
+    {
+        yield return new WaitForSeconds(timeToDestruction);
+        Destroy(gameObjectToDestroy);
+    }
+}
