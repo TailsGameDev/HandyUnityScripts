957dd1a6a03df1a8f7802f2d5a08dd349a7fbd3b
Caina Correa Caldas <cainacc@hotmail.com>
2021-10-18 21:47:49 -0300

    TransformWrapper

---
+++ TransformWrapper.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class TransformWrapper
+{
+    private Transform transform;
+    private TransformWrapper parent;
+
+    public TransformWrapper(Transform transform)
+    {
+        this.transform = transform;
+    }
+
+    public Vector3 Position {
+        get {
+            return transform.position;
+        }
+        set {
+            transform.position = value;
+        }
+    }
+    public Vector3 LocalPosition {
+        get {
+            return transform.localPosition;
+        }
+        set {
+            transform.localPosition = value;
+        }
+    }
+
+    public Vector3 EulerAngles {
+        get {
+            return transform.eulerAngles;
+        }
+        set {
+            transform.eulerAngles = value;
+        }
+    }
+
+    public void Rotate(float x, float y, float z)
+    {
+        Vector3 rotation = new Vector3(x, y, z);
+        transform.Rotate(rotation);
+    }
+
+    public Vector3 LocalScale {
+        get {
+            return transform.localScale;
+        }
+        set {
+            transform.localScale = value;
+        }
+    }
+
+    public void SetParent(TransformWrapper parent, bool worldPositionStays = true)
+    {
+        // TODO: check if default worldPositionStays should really be true
+        Debug.LogError("Set Parent of: " + transform.gameObject.name + " to " + parent.transform.gameObject.name);
+        transform.SetParent(parent.transform, worldPositionStays: worldPositionStays);
+    }
+    public TransformWrapper Parent {
+        get {
+            if (this.parent == null && transform.parent != null)
+            {
+                this.parent = new TransformWrapper(transform.parent);
+            }
+            return this.parent;
+        }
+    }
+}
